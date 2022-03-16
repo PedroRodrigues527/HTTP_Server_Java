287400b3c6ed3801d8959a5b29077e181c40d398
JoaoCupido <jcupido13@gmail.com>
2022-03-16 17:39:48 +0000

    Create log thread.java

--- src/main/java/ServerLogThread.java
+++ src/main/java/ServerLogThread.java
@@ -1,16 +1,50 @@
 import java.util.concurrent.locks.ReentrantLock;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ServerLogThread extends Thread{
     ReentrantLock _lock;
+    String[] _data;
 
     public ServerLogThread(ReentrantLock lock, String[] data){
         _lock = lock;
+        _data = data;
     }
 
     @Override
     public void run(){
         _lock.lock();
-        //gravar ficheiro no ficheiro server.log
+        try {
+            File myObj = new File("server.log");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            while(true)
+            {
+                if(_data[0] != null) {
+                    FileWriter fw = new FileWriter(myObj.getName());
+
+                    LocalDateTime myDateObj = LocalDateTime.now();
+                    DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.ms");
+                    String formattedDate = myDateObj.format(myFormatObj);
+
+
+                    fw.write(formattedDate + "-Method:" + _data[0] + "-Route:" + _data[1] + "-");
+
+                    fw.close();
+                    System.out.println("Successfully wrote to the server.log");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         _lock.unlock();
     }
 
