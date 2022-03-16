8950400725d8ee564dd41082f6996ba7ddd299c4
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2022-03-16 17:34:56 +0000

    Creation of a lock to prevent alot of threads acessing the file

--- src/main/java/MainHTTPServerThread.java
+++ src/main/java/MainHTTPServerThread.java
@@ -12,6 +12,7 @@ public class MainHTTPServerThread extends Thread {
     private final String pathCupido = "\\Users\\jcupi\\Desktop\\IntelliJ_IDEA_Projects\\PROJETO_PA_1\\server";
     private final String pathDiogo = "\\Users\\Diogo\\IdeaProjects\\PROJETO_PA_1\\server";
 
+    ReentrantLock _lock;
     private DataInputStream in;
     private ServerSocket server;
     private Socket client;
