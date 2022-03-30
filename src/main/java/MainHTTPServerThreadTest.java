47d6cc3cc4b57b1651867de1e732fe5e658550c6
Diogo Santos <diogo_Henrique2001@hotmail.com>
2022-03-30 17:21:21 +0100

    Testes Unitários

--- src/main/java/MainHTTPServerThreadTest.java
+++ src/main/java/MainHTTPServerThreadTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.*;
 
 import java.io.File;
-
+import java.net.InetAddress;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainHTTPServerThreadTest {
@@ -34,8 +34,37 @@ class MainHTTPServerThreadTest {
         }
 
         @Nested
-        @DisplayName ("TextToLogThread Test")
+        @DisplayName ("TextToLogThread Test") //Testar se o texto é o expectável
         class TextToLogThreadTest {
+            private TextToLogThread ttlt;
+            private String[] _data;
+            private InetAddress ipclient;
+            private String conteudo;
+            @BeforeEach
+            void setUp()
+            {
+                _data = new String[]{"GET", "/user/profile/page.html"};
+                try{
+                    ipclient = InetAddress.getLocalHost();
+                    this.ttlt= new TextToLogThread(_data,ipclient.getHostAddress());
+                    ttlt.start();
+                    ttlt.join();
+                    if(ttlt.isReadyBool()){
+                        conteudo = ttlt.getText();
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+            @Test
+            @DisplayName("Test creation of Data String")
+            void testTextToLogThread(){
+                assertTrue(ttlt.isReadyBool());
+                assertTrue((conteudo).contains("-Method:"+_data[0]));
+                assertTrue((conteudo).contains("-Route:"+ _data[1]));
+                assertTrue((conteudo).contains("-/"+ipclient.getHostAddress()));
+
+            }
 
         }
 
